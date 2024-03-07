7db266451695b2df03b31007582d454148637d13
Vestel <ibrahim-akin@msn.com>
2024-03-07 08:13:56 +0300

    update

--- src/App.js
+++ src/App.js
@@ -57,14 +57,14 @@ export default function Game() {
     }
 
     const moves = history.map((squares, move) => (
-        <li key={move}>
+        <div key={move}>
             <button onClick={() => setCurrentMove(move)}>
                 {move > 0 ?
                     <><span lang-tag="go_to_move">{lang_xox[lang]['go_to_move']}</span>{move}</> :
                     <span lang-tag="go_to_start">{lang_xox[lang]['go_to_start']}</span>
                 }
             </button>
-        </li>
+        </div>
     ));
 
     const result = calculate(currentSquares);
@@ -76,13 +76,9 @@ export default function Game() {
             </div>
             <div className="game-info">
                 <div className="status">
-                    {result.winner ?
-                        <><span lang-tag="winner">{lang_xox[lang]['winner']}</span>{result.winner}</> :
-                        result.draw ? <span lang-tag="draw">{lang_xox[lang]['draw']}</span> :
-                            <><span lang-tag="next_player">{lang_xox[lang]['next_player']}</span>{xIsNext ? 'X' : 'O'}</>
-                    }
+                    <span lang-tag={result.status}>{lang_xox[lang][result.status]}</span>{result.winner ? result.winner : result.draw ? '' : <span>{xIsNext ? 'X' : 'O'}</span>}
                 </div>
-                <ul>{moves}</ul>
+                {moves}
             </div>
         </>
     );
@@ -90,15 +86,16 @@ export default function Game() {
 
 function calculate(squares) {
     const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-    let draw = true;
+    let status = 'draw', draw = true;
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return { winner: squares[a] };
+            return { status: 'winner', winner: squares[a] };
         }
         if (!squares[a] || !squares[b] || !squares[c]) {
+            status = 'next_player';
             draw = false;
         }
     }
-    return { draw };
+    return { status, draw };
 }
