9a104038982dc42b809d9660667752687d53f15c
Vestel <ibrahim-akin@msn.com>
2024-06-11 08:00:42 +0300

    update

--- src/lang/index.js
+++ src/lang/index.js
@@ -1,15 +1,21 @@
 export const lang_xox = {
     tr: {
         next_player: 'Sıradaki Oyuncu: ',
+        first_player: 'İlk Oyuncu: ',
         go_to_start: 'Oyun başlangıcına git',
         go_to_move: 'Hamleye git #',
+        start: 'Oyun başlangıcı',
+        move: 'Hamle #',
         winner: 'Kazanan: ',
         draw: 'Berabere'
     },
     en: {
         next_player: 'Next Player: ',
+        first_player: 'First Player: ',
         go_to_start: 'Go to game start',
         go_to_move: 'Go to move #',
+        start: 'Game start',
+        move: 'Move #',
         winner: 'Winner: ',
         draw: 'Draw'
     }
